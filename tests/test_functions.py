5db6eee533448aaa0dd64994b160bee706a7fa07
Nikita Tikhonov <nekitdevofficial@gmail.com>
2023-04-09 21:18:02 +0300

    Add `identity` test.

---
+++ tests/test_functions.py
@@ -0,0 +1,9 @@
+from typing import Any
+from hypothesis import given, strategies
+
+from funcs.functions import identity
+
+
+@given(strategies.from_type(type))
+def test_identity(item: Any) -> None:
+    assert identity(item) is item
